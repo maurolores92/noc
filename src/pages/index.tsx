86c753e0a5288be482660d753737f9055b8375d1
Mauricio <maurolores1992@gmail.com>
2024-05-29 12:59:08 -0300

    commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { Container, Typography } from '@mui/material'
 const Home = () => {
   return (
     <Container>
-      <Typography variant='h1'>home page</Typography>
+      <Typography variant='h1'>Pagina de Inicio</Typography>
     </Container>
   )
 }
