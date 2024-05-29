1ba1a5351f3f953f2aee92bb0004b4f0a5786752
Mauricio <maurolores1992@gmail.com>
2024-05-29 18:59:37 -0300

    ping

--- src/pages/ping/index.tsx
+++ src/pages/ping/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import {
   TextField,
   Button,
@@ -8,27 +8,27 @@ import {
   MenuItem,
   FormControl,
   InputLabel,
-  Grid
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Box
 } from '@mui/material'
+import { Icon } from '@iconify/react'
 
 const PingPage: React.FC = () => {
   const [ipAddress, setIpAddress] = useState<string>('')
   const [pingResults, setPingResults] = useState<any[]>([])
-  const [pingCount, setPingCount] = useState<number>(0)
   const [startPing, setStartPing] = useState<boolean>(false)
-  const [pingInterval, setPingInterval] = useState<number>(1000) // Nuevo estado para el intervalo de ping
+  const [pingInterval, setPingInterval] = useState<number>(1000)
 
   const handlePing = async () => {
     try {
       const response = await fetch(`/api/ping?ip=${ipAddress}`)
       if (response.ok) {
         const data = await response.json()
-        setPingResults(prevResults => {
-          const newResults = [data, ...prevResults]
-
-          // Añadimos una línea en blanco antes de la declaración de retorno
-          return newResults.slice(0, 15)
-        })
+        console.log(data)
+        setPingResults(prevResults => [data, ...prevResults].slice(0, 15))
       } else {
         console.error('Error fetching ping result:', response.statusText)
       }
@@ -41,19 +41,19 @@ const PingPage: React.FC = () => {
     let intervalId: NodeJS.Timeout | null = null
 
     if (startPing) {
-      intervalId = setInterval(() => {
-        if (pingCount) {
-          handlePing()
-        } else {
-          if (intervalId) clearInterval(intervalId)
-        }
-      }, pingInterval) // Usamos el estado pingInterval para el intervalo de ping
+      handlePing() // Llama a handlePing inmediatamente cuando startPing se establece en true
+      intervalId = setInterval(handlePing, pingInterval)
     }
 
     return () => {
       if (intervalId) clearInterval(intervalId)
     }
-  }, [ipAddress, pingCount, startPing, pingInterval]) // Agregamos pingInterval a las dependencias del useEffect
+  }, [ipAddress, startPing, pingInterval])
+
+  const startPingHandler = () => {
+    setPingResults([])
+    setStartPing(true)
+  }
 
   const stopPing = () => {
     setStartPing(false)
@@ -64,60 +64,56 @@ const PingPage: React.FC = () => {
       <Typography variant='h1' sx={{ fontSize: '30px' }} gutterBottom>
         Ping IP Address
       </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            label='IP Address'
-            variant='outlined'
-            value={ipAddress}
-            onChange={event => setIpAddress(event.target.value)}
-            fullWidth
-            required
-            autoFocus
-          />
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <FormControl fullWidth>
-            <InputLabel id='ping-interval-label'>Ping Interval</InputLabel>
-            <Select
-              labelId='ping-interval-label'
-              value={pingInterval}
-              onChange={event => setPingInterval(event.target.value as number)}
-            >
-              <MenuItem value={250}>250ms</MenuItem>
-              <MenuItem value={500}>500ms</MenuItem>
-              <MenuItem value={700}>700ms</MenuItem>
-              <MenuItem value={1000}>1000ms</MenuItem>
-            </Select>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Button
-            onClick={() => {
-              setStartPing(false)
-              setPingResults([])
-              setPingCount(0)
-              setTimeout(() => setStartPing(true), 0)
-            }}
-            variant='contained'
-            color='primary'
-            fullWidth
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <TextField
+          label='IP Address'
+          variant='outlined'
+          value={ipAddress}
+          onChange={event => setIpAddress(event.target.value)}
+          fullWidth
+          required
+          autoFocus
+        />
+        <FormControl fullWidth sx={{ width: '40%', marginLeft: '1rem' }}>
+          <InputLabel id='ping-interval-label'>Ping Interval</InputLabel>
+          <Select
+            labelId='ping-interval-label'
+            value={pingInterval}
+            onChange={event => setPingInterval(event.target.value as number)}
           >
-            Ping
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Button onClick={stopPing} variant='contained' color='secondary' fullWidth>
-            Parar
-          </Button>
-        </Grid>
-      </Grid>
+            <MenuItem value={250}>250ms</MenuItem>
+            <MenuItem value={500}>500ms</MenuItem>
+            <MenuItem value={700}>700ms</MenuItem>
+            <MenuItem value={1000}>1000ms</MenuItem>
+          </Select>
+        </FormControl>
+        <Button
+          onClick={startPingHandler}
+          variant='contained'
+          color='primary'
+          sx={{ margin: '0 1rem', padding: '0 2rem' }}
+        >
+          Ping
+        </Button>
+        <Button onClick={stopPing} variant='contained' color='secondary' sx={{ margin: '0 1rem', padding: '0 2rem' }}>
+          Parar
+        </Button>
+      </Box>
       {pingResults.map((result, index) => (
-        <div key={index}>
-          <Typography variant='body1' gutterBottom>
-            {result.alive ? `Ping successful tiempo= ${result.time}ms` : 'Host is unreachable'}
-          </Typography>
-        </div>
+        <Fragment key={index}>
+          <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+            <ListItem disablePadding>
+              <ListItemIcon>
+                <Icon icon='mdi:internet-protocol' fontSize={20} />
+              </ListItemIcon>
+              <ListItemText>
+                {result.alive
+                  ? `Ping successful tiempo= ${result.time}ms, bytes= ${result.output.match(/bytes=(\d+)/)[1]} bytes`
+                  : 'Tiempo de espera agotado para esta solicitud.'}
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Fragment>
       ))}
     </Container>
   )
