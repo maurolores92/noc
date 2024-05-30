7aae5946448401bab15cd9495fecb64c7c59eaf3
Mauricio <maurolores1992@gmail.com>
2024-05-29 21:14:21 -0300

    ping

--- src/pages/ping/index.tsx
+++ src/pages/ping/index.tsx
@@ -12,7 +12,8 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Box
+  Box,
+  Divider
 } from '@mui/material'
 import { Icon } from '@iconify/react'
 
@@ -27,7 +28,7 @@ const PingPage: React.FC = () => {
       const response = await fetch(`https://chipped-sophisticated-grey.glitch.me/ping/${ipAddress}`)
       if (response.ok) {
         const data = await response.json()
-        setPingResults(prevResults => [data, ...prevResults].slice(0, 15))
+        setPingResults(prevResults => [data, ...prevResults]) // .slice(0, 9) ha sido removido
       } else {
         console.error('Error fetching ping result:', response.statusText)
       }
@@ -47,7 +48,7 @@ const PingPage: React.FC = () => {
     return () => {
       if (intervalId) clearInterval(intervalId)
     }
-  }, [ipAddress, startPing, pingInterval])
+  }, [startPing, pingInterval]) // ipAddress ha sido removido de las dependencias
 
   const startPingHandler = () => {
     setPingResults([])
@@ -60,7 +61,7 @@ const PingPage: React.FC = () => {
 
   return (
     <Container maxWidth='md'>
-      <Typography variant='h1' sx={{ fontSize: '30px' }} gutterBottom>
+      <Typography variant='h1' sx={{ fontSize: '20px' }} gutterBottom>
         Ping IP Address
       </Typography>
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -98,24 +99,100 @@ const PingPage: React.FC = () => {
           Parar
         </Button>
       </Box>
-      {pingResults.map((result, index) => (
-        <Fragment key={index}>
-          <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
-            <ListItem disablePadding>
-              <ListItemIcon>
-                <Icon icon='mdi:internet-protocol' fontSize={20} />
-              </ListItemIcon>
-              <ListItemText>
-                {result.alive
-                  ? `Ping successful tiempo= ${result.time}ms, bytes= ${
-                      (result.output.match(/bytes=(\d+)/) || [])[1] || 'N/A'
-                    } bytes`
-                  : 'Tiempo de espera agotado para esta solicitud.'}
-              </ListItemText>
-            </ListItem>
-          </List>
-        </Fragment>
-      ))}
+      <Box sx={{ height: '410px' }}>
+        <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+          {pingResults.slice(0, 9).map((result, index) => (
+            <Fragment key={index}>
+              <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+                <ListItem disablePadding>
+                  <ListItemIcon>
+                    <Icon icon='mdi:internet-protocol' fontSize={20} />
+                  </ListItemIcon>
+                  <ListItemText>
+                    {result.alive
+                      ? `Ping successful tiempo= ${Number(result.time).toFixed(1)}ms, bytes= ${
+                          (result.output.match(/(\d+) bytes from/) || [])[1] || 'N/A'
+                        } bytes, TTL= ${(result.output.match(/ttl=(\d+)/) || [])[1] || 'N/A'}`
+                      : 'Tiempo de espera agotado para esta solicitud.'}
+                  </ListItemText>
+                </ListItem>
+              </List>
+            </Fragment>
+          ))}
+        </List>
+      </Box>
+      <Divider sx={{ m: '0 !important' }} />
+      {pingResults.length > 0 && (
+        <Box sx={{ marginTop: '1rem' }}>
+          <Fragment>
+            <Typography variant='h4' fontWeight={800}>
+              Estadísticas de ping:
+            </Typography>
+            <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+              <ListItem disablePadding>
+                <ListItemIcon>
+                  <Icon icon='mdi:internet-protocol' fontSize={20} />
+                </ListItemIcon>
+                <ListItemText>
+                  Paquetes: enviados = {pingResults.length}, recibidos ={' '}
+                  {pingResults.filter(result => result.alive).length}
+                </ListItemText>
+              </ListItem>
+            </List>
+            <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+              <ListItem disablePadding>
+                <ListItemIcon>
+                  <Icon icon='mdi:internet-protocol' fontSize={20} />
+                </ListItemIcon>
+                <ListItemText>
+                  Paquetes perdidos = {pingResults.filter(result => !result.alive).length} (
+                  {((pingResults.filter(result => !result.alive).length / pingResults.length) * 100).toFixed(1)}% )
+                </ListItemText>
+              </ListItem>
+            </List>
+            <Typography sx={{ margin: '1rem 0' }}>Tiempos aproximados de ida y vuelta en ms:</Typography>
+            <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+              <ListItem disablePadding>
+                <ListItemIcon>
+                  <Icon icon='mdi:internet-protocol' fontSize={20} />
+                </ListItemIcon>
+                <ListItemText>
+                  Mínimo = {Math.min(...pingResults.filter(result => result.alive).map(result => Number(result.time)))}
+                  ms
+                </ListItemText>
+              </ListItem>
+            </List>
+            <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+              <ListItem disablePadding>
+                <ListItemIcon>
+                  <Icon icon='mdi:internet-protocol' fontSize={20} />
+                </ListItemIcon>
+                <ListItemText>
+                  Máximo = {Math.max(...pingResults.filter(result => result.alive).map(result => Number(result.time)))}
+                  ms
+                </ListItemText>
+              </ListItem>
+            </List>
+            <List component='nav' aria-label='main mailbox' sx={{ textAlign: 'center', margin: '0 auto' }}>
+              <ListItem disablePadding>
+                <ListItemIcon>
+                  <Icon icon='mdi:internet-protocol' fontSize={20} />
+                </ListItemIcon>
+                <ListItemText>
+                  Media ={' '}
+                  {(
+                    pingResults
+                      .filter(result => result.alive)
+                      .reduce((total, result) => total + Number(result.time), 0) /
+                    pingResults.filter(result => result.alive).length
+                  ).toFixed(1)}
+                  ms
+                </ListItemText>
+              </ListItem>
+            </List>
+          </Fragment>
+        </Box>
+      )}
     </Container>
   )
 }
